b4fbb3caf56dfe218583da07cbc696c385d41c3b
frahman27 <158252946+frahman27@users.noreply.github.com>
2025-05-13 20:07:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ const tracks = [
   'assets/clip1.mp3',
   'assets/clip2.mp3',
   'assets/clip3.mp3',
-  'assets/clip4.mp3'
+  'assets/clip4.mp3',
+  'assets/clip5.mp3',
+  'assets/clip6.mp3',
+  'assets/clip7.mp3',
+  'assets/clip8.mp3',
+  'assets/clip9.mp3'
 ];
 
 let current = 0;
